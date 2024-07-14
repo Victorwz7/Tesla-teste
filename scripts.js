28da4d53b6834edce004bc5742a29adb03400e46
VICTOR DOUSSEAU SOARES DA SILVA <victordousseau027@gmail.com>
2024-07-13 22:19:19 -0300

    Tesla teste

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".Formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+}
