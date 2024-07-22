bc67cb782f3620f48e6771978962b76de6b9bc02
VICTOR DOUSSEAU SOARES DA SILVA <victordousseau027@gmail.com>
2024-07-22 13:42:55 -0300

    Add files via upload

---
+++ Tesla teste/scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".Formulario");
+
+function aparecerFormulario() {
+    formulario.classList.add("visible");
+}
+
+function desaparecerFormulario() {
+    formulario.classList.remove("visible");
+}
